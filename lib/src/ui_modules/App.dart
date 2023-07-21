3934d2b78b1d4004f4632d36c97a2c052f33cec8
Agraj Parashar <agraj.parashar@unthinkable.co>
2023-07-21 15:32:00 +0530

    intial

---
+++ lib/src/ui_modules/App.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class App extends StatelessWidget {
+  static final navKey = new GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
